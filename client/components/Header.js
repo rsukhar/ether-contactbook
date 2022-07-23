0cba8a9821af06e556847ab13cc8aeb3b4c06565
rsukhar <ruslan@sukhar.info>
2022-07-23 18:22:49 +0300

    Added login buttin

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,7 +1,25 @@
-import {Menu} from "semantic-ui-react";
+import {Button, Menu} from "semantic-ui-react";
 import Link from "next/link";
+import provider from "../provider";
+import {useState} from "react";
 
 const Header = () => {
+	const [currentAccount, setCurrentAccount] = useState('');
+
+	const handleLoginClick = async (event) => {
+		event.preventDefault();
+		const {ethereum} = window;
+		if ( ! ethereum){
+			alert('You dont have metamask');
+		}
+		// TODO check of the proper network
+		try {
+			const accounts = await ethereum.request({method: 'eth_requestAccounts'});
+			setCurrentAccount(accounts[0]);
+		} catch (error) {
+			console.error(error);
+		}
+	};
 	return (<>
 		<Menu style={{marginTop: '20px'}}>
 			<Link href="/">
@@ -19,6 +37,12 @@ const Header = () => {
 					Посмотреть контакт
 				</Menu.Item>
 			</Link>
+			<Menu.Item position="right">
+				{currentAccount ? <Link href="/user/">
+						<Button primary onClick={handleLoginClick}>{currentAccount}</Button>
+					</Link> :
+				<Button primary onClick={handleLoginClick}>Login</Button>}
+			</Menu.Item>
 		</Menu>
 	</>);
 };
